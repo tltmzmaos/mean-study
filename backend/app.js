4d1acd42fe2dbbcf9f815f7b32d2cc88d57d028d
tltmzmaos <tltmzmaos@gmail.com>
2022-05-22 18:58:02 -0700

    Added MongoDB Connection

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://jm:VF1UxetkGb3sHd9H@cluster0.wuuse.mongodb.net/node-angualr?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch(() => {
+    console.log('Connection failed!');
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,21 +25,33 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/posts', (req, res, next) => {
-  const post = req.body;
-  console.log(post);
-  res.status(201).json({
-    message: 'Post Added Successfully'
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
   });
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: 'Post Added Successfully',
+      postId: createdPost._id
+    });
+  });
+
 });
 
-app.use('/api/posts', (req, res, next) => {
-  const posts = [
-    {id: '1', title: 'First server-side post', content:'This is from nodejs'},
-    {id: '2', title: 'Second server-side post', content:'This is from nodejs'}
-  ];
-  return res.status(200).json({
-    message: 'Posts fetched successfully!',
-    posts: posts
+app.get('/api/posts', (req, res, next) => {
+  Post.find()
+    .then(document => {
+      res.status(200).json({
+        message: 'Posts fetched successfully!',
+        posts: document
+      });
+    });
+});
+
+app.delete('/api/posts/:id', (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then(result => {
+    console.log(result);
+    res.status(200).json({message: 'Post deleted!'});
   });
 });
 
